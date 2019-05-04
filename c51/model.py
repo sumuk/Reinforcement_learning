3c13bbe28ee9ab867004f36736ebfc0c0e9e672c
sumukh badam <sumukbadam@gmail.com>
2019-05-04 16:32:04 +0530

    added stable c51 algo

---
+++ c51/model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, num_nodes, seed, fc1_units=256, fc2_units=256):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.num_nodes = num_nodes
+        self.action_size = action_size
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size * num_nodes)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = x.view((-1,self.action_size,self.num_nodes))
+        x1 = F.softmax(x,dim=-1)
+        x2 = F.log_softmax(x,dim=-1)
+        return x1, x2
